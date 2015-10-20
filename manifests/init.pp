73c323b5184454eb25ad20a3fef3c89a6ee4ae3c
Matt <matthew.a.nicholson@gmail.com>
2015-10-20 14:45:54 -0400

    start docker_toolbox app

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,9 @@
 # This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class docker_toolbox {
+  package { 'Docker_Toolbox':
+#    provider => 'compressed_app',
+    provider => 'pkgdmg',
+    source   => 'https://github.com/docker/toolbox/releases/download/v1.8.3/DockerToolbox-1.8.3.pkg',
+    #flavor   => 'zip',
+  }
 }
